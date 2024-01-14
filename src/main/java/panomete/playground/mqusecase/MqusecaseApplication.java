4dced5c9d8e49c7de63214020971f2b0ef6cbcd5
oat431 <oat431@gmail.com>
2024-01-14 22:11:47 +0700

    🎉 setup: mq usecase with spb3

---
+++ src/main/java/panomete/playground/mqusecase/MqusecaseApplication.java
@@ -0,0 +1,13 @@
+package panomete.playground.mqusecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqusecaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqusecaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/panomete/playground/mqusecase/MqusecaseApplicationTests.java
@@ -0,0 +1,13 @@
+package panomete.playground.mqusecase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqusecaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
