30bba0791beffb6b297998669ca897af78d33c48
oat431 <oat431@gmail.com>
2024-01-17 22:16:22 +0700

    🔨 add: kafka annotation

--- src/main/java/panomete/playground/mqusecase/MqusecaseApplication.java
+++ src/main/java/panomete/playground/mqusecase/MqusecaseApplication.java
@@ -2,7 +2,9 @@ package panomete.playground.mqusecase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class MqusecaseApplication {
 
